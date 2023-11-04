480248649a709d4a79785128934ec93e3f9c4290
neeraj-sharma2592 <63321305+neeraj-sharma2592@users.noreply.github.com>
2023-11-04 16:56:53 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+        data: {
+            name: "Nodeapp"
+        }
+    })
+});
+
+app.listen(3000, () => {});
